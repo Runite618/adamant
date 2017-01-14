320a765153fc7c305d30b1fd986f65391a10a648
Matthew Mo <matthew_mo@hotmail.co.uk>
2017-01-14 12:48:59 +0000

    Allows user to specify file and print contents to screen.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace adamant
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Provide a file to read into the text string variable");
+
+            var fileName = Console.ReadLine();
+            string text;
+
+            if (File.Exists(fileName))
+            {
+                text = File.ReadAllText(fileName);
+                Console.WriteLine(text);
+                Console.ReadLine();
+            }
         }
     }
 }
